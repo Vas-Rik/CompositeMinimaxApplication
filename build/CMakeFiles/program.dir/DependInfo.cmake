dec1e63d63ea485119b507590e936c94ed4c2554
Vas-Rik <vasco.rikkers@gmail.com>
2025-02-18 16:08:43 +0000

    Added build folder

---
+++ build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/demo.cpp" "CMakeFiles/program.dir/src/demo.cpp.o" "gcc" "CMakeFiles/program.dir/src/demo.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/func.cpp" "CMakeFiles/program.dir/src/func.cpp.o" "gcc" "CMakeFiles/program.dir/src/func.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/minimum.cpp" "CMakeFiles/program.dir/src/minimum.cpp.o" "gcc" "CMakeFiles/program.dir/src/minimum.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/optimized_degrees.cpp" "CMakeFiles/program.dir/src/optimized_degrees.cpp.o" "gcc" "CMakeFiles/program.dir/src/optimized_degrees.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/ranking.cpp" "CMakeFiles/program.dir/src/ranking.cpp.o" "gcc" "CMakeFiles/program.dir/src/ranking.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/ranking_compos.cpp" "CMakeFiles/program.dir/src/ranking_compos.cpp.o" "gcc" "CMakeFiles/program.dir/src/ranking_compos.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/sorting.cpp" "CMakeFiles/program.dir/src/sorting.cpp.o" "gcc" "CMakeFiles/program.dir/src/sorting.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/utils-basics.cpp" "CMakeFiles/program.dir/src/utils-basics.cpp.o" "gcc" "CMakeFiles/program.dir/src/utils-basics.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/utils-eval.cpp" "CMakeFiles/program.dir/src/utils-eval.cpp.o" "gcc" "CMakeFiles/program.dir/src/utils-eval.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/utils-matrices.cpp" "CMakeFiles/program.dir/src/utils-matrices.cpp.o" "gcc" "CMakeFiles/program.dir/src/utils-matrices.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/utils-ptxt.cpp" "CMakeFiles/program.dir/src/utils-ptxt.cpp.o" "gcc" "CMakeFiles/program.dir/src/utils-ptxt.cpp.o.d"
+  "/home/kali/Desktop/thesis/fhe_efficient_approx_pipeline/src/utils-remez.cpp" "CMakeFiles/program.dir/src/utils-remez.cpp.o" "gcc" "CMakeFiles/program.dir/src/utils-remez.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/src/demo.cpp.o"
+  "CMakeFiles/program.dir/src/demo.cpp.o.d"
+  "CMakeFiles/program.dir/src/func.cpp.o"
+  "CMakeFiles/program.dir/src/func.cpp.o.d"
+  "CMakeFiles/program.dir/src/minimum.cpp.o"
+  "CMakeFiles/program.dir/src/minimum.cpp.o.d"
+  "CMakeFiles/program.dir/src/optimized_degrees.cpp.o"
+  "CMakeFiles/program.dir/src/optimized_degrees.cpp.o.d"
+  "CMakeFiles/program.dir/src/ranking.cpp.o"
+  "CMakeFiles/program.dir/src/ranking.cpp.o.d"
+  "CMakeFiles/program.dir/src/ranking_compos.cpp.o"
+  "CMakeFiles/program.dir/src/ranking_compos.cpp.o.d"
+  "CMakeFiles/program.dir/src/sorting.cpp.o"
+  "CMakeFiles/program.dir/src/sorting.cpp.o.d"
+  "CMakeFiles/program.dir/src/utils-basics.cpp.o"
+  "CMakeFiles/program.dir/src/utils-basics.cpp.o.d"
+  "CMakeFiles/program.dir/src/utils-eval.cpp.o"
+  "CMakeFiles/program.dir/src/utils-eval.cpp.o.d"
+  "CMakeFiles/program.dir/src/utils-matrices.cpp.o"
+  "CMakeFiles/program.dir/src/utils-matrices.cpp.o.d"
+  "CMakeFiles/program.dir/src/utils-ptxt.cpp.o"
+  "CMakeFiles/program.dir/src/utils-ptxt.cpp.o.d"
+  "CMakeFiles/program.dir/src/utils-remez.cpp.o"
+  "CMakeFiles/program.dir/src/utils-remez.cpp.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
